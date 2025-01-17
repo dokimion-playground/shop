4b8821a81e78f20c525cf885af02bc0f0d76454f
dokimion24 <leecy3610@gmail.com>
2025-01-17 09:10:52 +0900

    feat: 로그아웃 버튼 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
-import { HomeIcon, File, UsersRound } from "lucide-react";
+import { HomeIcon, File, UsersRound, LogOut } from "lucide-react";
 import Link from "next/link";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+
+import { Button } from "@/components/ui/button";
 import { NavButton } from "@/components/NavButton";
 import { ModeToggle } from "./ModeToggle";
 
@@ -21,6 +24,18 @@ export function Header() {
           <NavButton href="/tickets" label="Tickets" icon={File} />
           <NavButton href="/customers" label="Customers" icon={UsersRound} />
           <ModeToggle />
+          <Button
+            variant="ghost"
+            size="icon"
+            aria-label="LogOut"
+            title="LogOut"
+            className="rounded-full"
+            asChild
+          >
+            <LogoutLink>
+              <LogOut />
+            </LogoutLink>
+          </Button>
         </div>
       </div>
     </header>
