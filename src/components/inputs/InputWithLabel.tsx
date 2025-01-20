a32eae90ec5b0fe387c2d619b8296871045f6969
dokimion24 <leecy3610@gmail.com>
2025-01-20 18:20:17 +0900

    feat: add InputWithLabel component

---
+++ src/components/inputs/InputWithLabel.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useFormContext } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { InputHTMLAttributes } from "react";
+import { Input } from "../ui/input";
+
+type Props<S> = {
+  fieldTitle: string;
+  nameInSchema: keyof S & string;
+  className?: string;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+export function InputWithLabel<S>({
+  fieldTitle,
+  nameInSchema,
+  className,
+  ...props
+}: Props<S>) {
+  const form = useFormContext();
+
+  return (
+    <FormField
+      control={form.control}
+      name={nameInSchema}
+      render={({ field }) => (
+        <FormItem className="text-base">
+          <FormLabel htmlFor={nameInSchema}>{fieldTitle}</FormLabel>
+          <FormControl>
+            <Input
+              id={nameInSchema}
+              className={`w-full max-w-xs disabled:text-blue-500 dark:disabled:text-green-500 disabled:opacity-70 ${className}`}
+              {...props}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+}
