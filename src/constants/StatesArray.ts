b061d1c4de7a9853cdd64295b4b2442f8fe9de08
dokimion24 <leecy3610@gmail.com>
2025-01-20 21:03:57 +0900

    feat: add StatesArray

---
+++ src/constants/StatesArray.ts
@@ -0,0 +1,21 @@
+const StatesArray = [
+  { id: "GG", description: "경기도" },
+  { id: "GW", description: "강원도" },
+  { id: "CB", description: "충청북도" },
+  { id: "CN", description: "충청남도" },
+  { id: "JB", description: "전라북도" },
+  { id: "JN", description: "전라남도" },
+  { id: "GB", description: "경상북도" },
+  { id: "GN", description: "경상남도" },
+  { id: "JJ", description: "제주특별자치도" },
+  { id: "SO", description: "서울특별시" },
+  { id: "BS", description: "부산광역시" },
+  { id: "DG", description: "대구광역시" },
+  { id: "IC", description: "인천광역시" },
+  { id: "GJ", description: "광주광역시" },
+  { id: "DJ", description: "대전광역시" },
+  { id: "US", description: "울산광역시" },
+  { id: "SJ", description: "세종특별자치시" },
+] as const;
+
+export default StatesArray;
